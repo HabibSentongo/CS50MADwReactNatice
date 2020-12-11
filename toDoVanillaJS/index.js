293a656c06e870fe4e8ddeb5d87a007d2f011408
habibsentongo <habibsentongo@gmail.com>
2020-12-11 07:38:15 +0300

    improvement on the vanillaJS ToDo App

--- toDoVanillaJS/index.js
+++ toDoVanillaJS/index.js
@@ -1,6 +1,18 @@
 function todoAdd() {
     todolist = document.getElementById("todo")
-    todoText = document.getElementById("newText")
-    todolist.innerHTML += "<li>" + todoText.value + "</li>"
+    todoText = document.getElementById("newTodo")
+    todolist.innerHTML = ("<li>" + todoText.value + "</li>") + todolist.innerHTML
     todoText.value = ""
+}
+
+function taskAdd() {
+    tasklist = document.getElementById("tasks")
+    taskText = document.getElementById("newTask")
+    tasklist.innerHTML = ("<li>" + taskText.value + "</li>") + tasklist.innerHTML
+    taskText.value = ""
+}
+
+function showTasks() {
+    tasks = document.getElementById("insideTodo")
+    tasks.classList.toggle("show")
 }
