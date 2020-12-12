f877345fdab46fdf3b4a559df7d06fdffd8889b8
habibsentongo <habibsentongo@gmail.com>
2020-12-12 11:41:15 +0300

    vanillaJS ToDo app improvement

--- toDoVanillaJS/index.js
+++ toDoVanillaJS/index.js
@@ -1,18 +1,36 @@
+idCounter = 1
 function todoAdd() {
     todolist = document.getElementById("todo")
     todoText = document.getElementById("newTodo")
-    todolist.innerHTML = ("<li>" + todoText.value + "</li>") + todolist.innerHTML
+    idCounter += 1
+    newlist = ('<li class="taskList">' +
+                    '<span class="clickable" onclick="showTasks(`insideTodo' + idCounter + '`)">' + todoText.value + '</span>' +
+                    '<div class="tasksdiv" id="insideTodo' + idCounter+ '">' +
+                        '<ul id="tasks' + idCounter + '">' +
+                        '</ul>' +
+                        '<ul>' +
+                            '<li class="createForm">' +
+                                '<form onsubmit="taskAdd(`newTask' + idCounter + '`, `tasks' + idCounter + '`); return false">' +
+                                    '<div>' +
+                                        '<input type="text" placeholder="New task to be done ..." id="newTask' + idCounter + '" required>' +
+                                    '</div>' +
+                                '</form>' +
+                            '</li>' +
+                        '</ul>' +
+                    '</div>' +
+                '</li>' )
+    todolist.innerHTML += newlist
     todoText.value = ""
 }
 
-function taskAdd() {
-    tasklist = document.getElementById("tasks")
-    taskText = document.getElementById("newTask")
-    tasklist.innerHTML = ("<li>" + taskText.value + "</li>") + tasklist.innerHTML
+function taskAdd(textboxID, listID) {
+    tasklist = document.getElementById(listID)
+    this.taskText = document.getElementById(textboxID)
+    this.tasklist.innerHTML += ("<li>" + taskText.value + "</li>")
     taskText.value = ""
 }
 
-function showTasks() {
-    tasks = document.getElementById("insideTodo")
+function showTasks(listID) {
+    tasks = document.getElementById(listID)
     tasks.classList.toggle("show")
 }
